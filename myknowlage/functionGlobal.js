1969d827ddc6205570ce0e4e6e23ca00d9822646
SahilMern <sahil9082811@gmail.com>
2024-11-05 21:42:36 +0530

    adding global context details

---
+++ myknowlage/functionGlobal.js
@@ -0,0 +1,10 @@
+globalExcicution()
+function globalExcicution() {
+    console.log("gello");
+}
+//TODO:- It work but 
+
+globalExcicution1()
+const  globalExcicution1= () =>  {
+    console.log("gello");
+}
