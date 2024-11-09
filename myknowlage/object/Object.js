b90dc0db70cfc10fc64f6f13b2286951945a3255
SahilMern <sahil9082811@gmail.com>
2024-11-09 18:47:25 +0530

    Adding obect .craete

--- myknowlage/object/Object.js
+++ myknowlage/object/Object.js
@@ -1,8 +1,11 @@
 //TODO:- Possible Way to create Object in js
+//? 1)In JavaScript, the Object.create() method is used to create a new object, using an existing object as the prototype of the newly created object.
 const person = {
   greet: function () {
     console.log("Hello!");
   },
 };
 const john = Object.create(person);
+console.log(john);
+
 john.greet(); // Output: Hello!
