849e6e40fe83a7d4b7be19bd5f5cf01168fac8f2
SahilMern <sahil9082811@gmail.com>
2024-11-12 11:31:51 +0530

    String method

---
+++ methods/String/string.js
@@ -0,0 +1,8 @@
+const name = "Hello very Good afternoon";
+console.log(name.length, "length");
+
+console.log(name.charAt(0), "charAt");
+console.log(name.at(0), "charAt");
+console.log(name.charCodeAt(0), "charAt");
+
+
