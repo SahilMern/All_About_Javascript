a9c44d0c1878c303977705197ed4256ac4d2862b
SahilMern <sahil9082811@gmail.com>
2024-11-13 13:11:51 +0530

    adding call

--- OopsJavascript/Constructor/call.js
+++ OopsJavascript/Constructor/call.js
@@ -1,26 +1,26 @@
-function Person(name, age) {
-    this.name = name;
+function Person(name, age){
+    this.name= name;
     this.age = age;
 }
 
 Person.prototype.getDetails = function() {
     console.log(`Hello My name is ${this.name} and My Age is ${this.age}`);
-};
-
-function Employe(name, age, jobTitle, salary) {
-    Person.call(this, name, age); // Call parent constructor with 'name' and 'age'
-    this.jobTitle = jobTitle;
-    this.salary = salary;
+    
 }
 
-// Set the prototype chain so that Employe instances inherit from Person
-Employe.prototype = Object.create(Person.prototype);
-// Employe.prototype.constructor = Employe;
+function Employe(jobTitle, salary){
+    Person.call(this, jobTitle, salary)
+    this.jobTitle = jobTitle
+    this.salary =salary
+}
 
-const newEmploye = new Employe("Sahil", 22, "MERN Developer", 1000000);
-console.log(newEmploye);  // This will log the newEmploye object
-newEmploye.getDetails();  // This will call the getDetails method from the Person prototype
+Employe.prototype = Object.create(Person.prototype)
+Employe.prototype.constructor  = Employe;
 
+
+const newEmploye = new Employe("sahil", 22, "Mern Developer",1000000);
+console.log(newEmploye);
+console.log(newEmploye.getDetails());
 
 //! Employe.prototype = Object.create(Person.prototype);
 //?  yeh line ensure karti hai ki Employe class Person class ke prototype methods (jaise getDetails()) inherit kar sake.
--- OopsJavascript/Constructor/test.js
+++ OopsJavascript/Constructor/test.js
@@ -1,25 +1,18 @@
-function BankAccount(customerName,balance){
-    this.customerName=customerName
-    this.accountNumber= Date.now()
-    this.balance = balance
-}
-
-const rakeshAccount = new BankAccount("Rakesh Account", 1000)
-const sahilAccount = new BankAccount("Sahil Account", 1000)
-console.log(rakeshAccount);
-console.log(sahilAccount);
-
-console.log(BankAccount.prototype, "BankAccount prototype"); //Empty Object
-// BankAccount.prototype.test = "Hello i am prototype"
-BankAccount.prototype.deposit = function(amount) {
-   this.balance += amount
-}
-
-BankAccount.prototype.withraw = function(amount) {
-    this.balance -= amount
- }
-
-sahilAccount.deposit(89000)
-rakeshAccount.withraw(8)
-console.log(sahilAccount, rakeshAccount);
-
+// Parent constructor function
+function Animal(name, food) {
+    this.name = name;
+    this.food=food
+  }
+  
+  // Child constructor function
+  function Dog(name, food,breed) {
+    // Using `call` to invoke the parent constructor
+    Animal.call(this, name, food);  // Sets `this` to the current Dog instance and passes the name argument
+    this.breed = breed;       // Additional property specific to Dog
+  }
+  
+  const myDog = new Dog("Buddy","pedigree", "Golden Retriever");
+  console.log(myDog.name);   // Output: Buddy
+  console.log(myDog.food);   // Output: Buddy
+  console.log(myDog.breed);  // Output: Golden Retriever
+  
