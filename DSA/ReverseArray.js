011de3b9c9b8cd9e38d6cfac86736ea3f7251919
SahilMern <sahil9082811@gmail.com>
2024-11-24 19:15:48 +0530

    adding reversed array

---
+++ DSA/ReverseArray.js
@@ -0,0 +1,52 @@
+function reverseArray(arr) {
+    const reversedArr = [];
+    
+    for (let i = arr.length - 1; i >= 0; i--) {
+      reversedArr.push(arr[i]);
+    }
+  
+    return reversedArr;
+  }
+  
+  // Example usage:
+  const originalArray = [1, 2, 3, 4, 5];
+  const reversedArray = reverseArray(originalArray);
+  
+  console.log(reversedArray); // Output: [5, 4, 3, 2, 1]
+  
+
+  // ruf----
+  function reverseArray(arr) {
+    const reversedArr = [];
+    
+    for (let i = arr.length - 1; i >= 0; i--) {
+      reversedArr.push(arr[i]);
+    }
+  
+    return reversedArr;
+  }
+  
+  // Example usage:
+  const originalArray = [1, 2, 3, 4, 5];
+  const reversedArray = reverseArray(originalArray);
+  
+  console.log(reversedArray); // Output: [5, 4, 3, 2, 1]
+  
+  // ruf --
+  function reverseArray(arr) {
+    const reversedArr = [];
+    
+    for (let i = arr.length - 1; i >= 0; i--) {
+      reversedArr.push(arr[i]);
+    }
+  
+    return reversedArr;
+  }
+  
+  // Example usage:
+  const originalArray = [1, 2, 3, 4, 5];
+  const reversedArray = reverseArray(originalArray);
+  
+  console.log(reversedArray); // Output: [5, 4, 3, 2, 1]
+  
+  
---
+++ DSA/findMinandMax.js
@@ -0,0 +1,10 @@
+function findMinMax(arr) {
+   console.log(Math.min(...arr));
+   console.log(Math.max(...arr));
+  }
+  
+  // Example usage:
+  const arr = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
+  const result = findMinMax(arr);
+//   console.log(`Min: ${result.min}, Max: ${result.max}`);
+  
