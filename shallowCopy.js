95a64261a6a0409134dc2ffc751df9cc80b80f56
SahilMern <sahil9082811@gmail.com>
2024-12-03 23:25:27 +0530

    shallow copy

---
+++ shallowCopy.js
@@ -0,0 +1,10 @@
+// Shallow Copy
+let original = { a: 1, b: { c: 2 } };
+let shallowCopy = { ...original };
+shallowCopy.b.c = 3;
+console.log(original.b.c); // 3 (because the reference is shared)
+
+// Deep Copy
+let deepCopy = JSON.parse(JSON.stringify(original));
+deepCopy.b.c = 4;
+console.log(original.b.c); // 2 (because the copy is independent)
