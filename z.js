5d008db724d2a33a427dce2ad7ebfad04119f6f5
Sahil yadav <sahil9082811@gmail.com>
2024-12-19 02:14:41 -0600

    check functionality of this keyword in js

--- z.js
+++ z.js
@@ -1,10 +1,2 @@
-const hof = (functionName, v1 , v2) => {
-    return functionName(v1, v2);
-}
+console.log(this);//=global
 
-const addition = (v1, v2) => {
-    return v1+v2
-}
-
-
-console.log(hof(addition, 1, 2));
