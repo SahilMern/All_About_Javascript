4b7c3fad4903fdd182a969a058585fb76387d5ca
Sahil yadav <sahil9082811@gmail.com>
2024-12-30 01:24:43 -0600

    practice reduce method

--- z.js
+++ z.js
@@ -1,21 +1,7 @@
-console.log(this);//=global
+// Sum USING Reduce method and return is imp for next itration
 
-//! STATE MUTAION
-let Obj = {
-    a:20,
-    b:30
-}
-console.log(Obj.a);
-Obj.a = 50
-console.log(Obj.a);
-
-// Shallow Copy
-let original = { a: 1, b: { c: 2 } };
-let shallowCopy = { ...original };
-shallowCopy.b.c = 3;
-console.log(original.b.c); // 3 (because the reference is shared)
-
-// Deep Copy
-let deepCopy = JSON.parse(JSON.stringify(original));
-deepCopy.b.c = 5;
-console.log(original.b.c); // 2 (because the copy is independent)
+const array = [1, 2, 3, 4, 2, 3, 5, 6];
+const sum = array.reduce((accu, elem, index) => {
+    return accu +elem
+}, 0)
+console.log(sum, "sum");
