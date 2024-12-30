1af94c347c3f7b91c5b81c36eb58b162b9ca3800
Sahil yadav <sahil9082811@gmail.com>
2024-12-30 01:28:35 -0600

    practice to find max value in array

--- z.js
+++ z.js
@@ -1,7 +1,14 @@
 // Sum USING Reduce method and return is imp for next itration
 
-const array = [1, 2, 3, 4, 2, 3, 5, 6];
-const sum = array.reduce((accu, elem, index) => {
-    return accu +elem
-}, 0)
-console.log(sum, "sum");
+const array = [1, 2, 3, 4,11, 2, 3, 5, 6];
+let maxValue = array[0]
+for (let index = 0; index < array.length; index++) {
+    console.log(array[index]);
+    
+    if(maxValue<array[index]){
+        maxValue= array[index]
+    }
+    
+}
+console.log(maxValue, "maxValue");
+
