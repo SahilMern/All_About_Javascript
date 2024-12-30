584f40385cd043689c4a558b1773cc613126098d
Sahil yadav <sahil9082811@gmail.com>
2024-12-30 01:49:28 -0600

    adding second largest value in array

--- z.js
+++ z.js
@@ -1,14 +1,27 @@
-// Sum USING Reduce method and return is imp for next itration
+const array = [1, 2, 9, 4, 11, 2, 3, 6, 5, 0, 2];
 
-const array = [1, 2, 3, 4,11, 2, 3, 5, 6];
-let maxValue = array[0]
-for (let index = 0; index < array.length; index++) {
-    console.log(array[index]);
-    
-    if(maxValue<array[index]){
-        maxValue= array[index]
+// Bubble Sort Algorithm
+for (let i = 0; i < array.length; i++) {
+  for (let j = 0; j < array.length - 1; j++) { // Fixing index out of bounds
+    if (array[j] < array[j + 1]) {
+      let temp = array[j];
+      array[j] = array[j + 1];
+      array[j + 1] = temp;
     }
-    
+  }
 }
-console.log(maxValue, "maxValue");
 
+console.log(array, "sorted array");
+
+// Find the largest and second largest distinct value
+let largest = array[0];
+let secondLargest = null;
+
+for (let i = 1; i < array.length; i++) {
+  if (array[i] < largest) {
+    secondLargest = array[i];
+    break;
+  }
+}
+
+console.log("Second largest distinct value:", secondLargest);
