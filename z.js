86ffde811b6cbc5c0828e374c622ede9256f9524
Sahil yadav <sahil9082811@gmail.com>
2024-12-30 02:04:59 -0600

    solving factorial probelm

--- z.js
+++ z.js
@@ -1,27 +1,11 @@
-const array = [1, 2, 9, 4, 11, 2, 3, 6, 5, 0, 2];
-
-// Bubble Sort Algorithm
-for (let i = 0; i < array.length; i++) {
-  for (let j = 0; j < array.length - 1; j++) { // Fixing index out of bounds
-    if (array[j] < array[j + 1]) {
-      let temp = array[j];
-      array[j] = array[j + 1];
-      array[j + 1] = temp;
-    }
+const findFactoprial = (num) => {
+  if (num < 0) {
+    console.log("err");
+    return false;
   }
-}
-
-console.log(array, "sorted array");
-
-// Find the largest and second largest distinct value
-let largest = array[0];
-let secondLargest = null;
-
-for (let i = 1; i < array.length; i++) {
-  if (array[i] < largest) {
-    secondLargest = array[i];
-    break;
+  if (num == 0 || num == 1) {
+    return 1;
   }
-}
-
-console.log("Second largest distinct value:", secondLargest);
+  return num * findFactoprial(num-1)
+};
+console.log(findFactoprial(5));
