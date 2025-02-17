37d71762fcc90e5ea6b42864525f0cfef4341afc
Sahil yadav <sahil9082811@gmail.com>
2025-02-17 04:30:22 -0600

    practicing memeorization annomiyese function currying

--- z.js
+++ z.js
@@ -1,2 +1,57 @@
-let data = [4,8,4,9,4,9,5]
-console.log([new Set(data)]); //let data = [4,8,4,9,4,9,5]
+// "use strict";
+const cache = {};
+const memorizationFunction = (a, b) => {
+  return a + b;
+};
+
+const userinput = (a, b) => {
+  let key = a + ":" + b;
+  if (!cache[key]) {
+    const dataValue = memorizationFunction(a, b);
+    cache[key] = dataValue;
+    console.log(cache, "cache");
+  } else {
+    console.log("hello");
+    console.log(cache[key]);
+    
+  }
+};
+
+// userinput(3, 5);
+// userinput(4, 5);
+
+
+const curryingFunction = (a) => {
+  return (b) => {
+    return (c)=>{
+      console.log(a +b+ c);
+    }
+  }
+}
+// curryingFunction(5)(6)(1)
+
+(() => {
+  console.log("IIFE");
+})()
+
+
+const sum = (a, b) => {
+  console.log(a+b , "Final answer in Sum");
+  return a+b
+}
+
+const min = (a, b) => {
+  console.log(a-b , "Final answer in Sum");
+  return a-b
+}
+
+const Hof = (a,b,callbackFunction) => {
+  callbackFunction(a, b)
+}
+
+// Hof(4,5,sum)
+// Hof(4,5,min)
+
+
+const fatArrowFunction = (a,b) => console.log(a+b, "a+b");
+// fatArrowFunction(10,10)
